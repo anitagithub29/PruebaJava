6f9da6380a85e789604d4f1d8438ca2c3618335c
Daniel Tortosa <danieltortosahita@gmail.com>
2021-07-23 19:08:56 +0200

    Añadida clase Animales e importamos en el main la clase.

---
+++ PruebaJava/src/main/java/Animales/Animales.java
@@ -0,0 +1,16 @@
+package Animales;
+
+public class Animales {
+
+    private String nombre = "", raza = "";
+
+    public Animales() {
+    }
+
+    public Animales(String nombre, String raza) {
+        this.nombre = nombre;
+        this.raza = raza;
+
+    }
+
+}
--- PruebaJava/src/main/java/com/cursoaepi/pruebajava/PruebaJava.java
+++ PruebaJava/src/main/java/com/cursoaepi/pruebajava/PruebaJava.java
@@ -1,5 +1,6 @@
-
 package com.cursoaepi.pruebajava;
+
+import Animales.Animales;
 
 /**
  *
@@ -7,13 +8,13 @@ package com.cursoaepi.pruebajava;
  */
 public class PruebaJava {
 
-
-    
     public static void main(String[] args) {
         System.out.println("Holiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
         System.out.println("Nuevo mensaje, prueba segundo commit");
         System.out.println("Otra línea más");
         System.out.println("Holiii x2");
+
+        Animales animales = new Animales("Perro", "Labrador");
     }
-    
+
 }
