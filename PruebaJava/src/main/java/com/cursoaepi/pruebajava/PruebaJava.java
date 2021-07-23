f860d086684ebfdb0ca4e96855192f10ccc43f3e
Daniel Tortosa <danieltortosahita@gmail.com>
2021-07-23 18:42:27 +0200

    //Creación del proyecto y creación de la clase principal.

---
+++ PruebaJava/src/main/java/com/cursoaepi/pruebajava/PruebaJava.java
@@ -0,0 +1,16 @@
+
+package com.cursoaepi.pruebajava;
+
+/**
+ *
+ * @author Dani
+ */
+public class PruebaJava {
+
+
+    
+    public static void main(String[] args) {
+        System.out.println("Holiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+    }
+    
+}
